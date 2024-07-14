f4ab4b5dfd78325295c9745c22eec575a9c67493
tanguy dugas <tanguy.dugas01@gmail.com>
2024-07-14 16:00:28 +0200

    rewrite reinforce upate

--- agent/reinforce_agent.py
+++ agent/reinforce_agent.py
@@ -11,18 +11,20 @@ class ReinforceAgent(Agent):
 
     def update_policy(self):
         """Update the policy at the end of an episode based on the reward obtained during it."""
-        action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.device).squeeze(-1)
-        rewards = torch.stack(self.rewards, dim=0).to(self.device).squeeze(-1)
-
-        discounted_r = discount_rewards(rewards, self.gamma) - self.baseline
-        loss = []
-        for R, log_prob in zip(discounted_r, action_log_probs):
-            loss.append(-R*log_prob*self.gamma)
-
-        loss = torch.stack(loss, dim=0).sum()
-
+        action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)
+        rewards=self.rewards
+        discounted_returns = []
+        dr = 0
+        for r in rewards[::-1]:
+            dr = dr*self.gamma + r
+            discounted_returns.append(dr)
+        discounted_returns.reverse()
+        discounted_returns = torch.stack(discounted_returns, dim=0).to(self.train_device).squeeze(-1)
+        discounted_returns = (discounted_returns - discounted_returns.mean())/ discounted_returns.std()
+        discounted_returns = discounted_returns - self.baseline
         self.policy_optimizer.zero_grad()
+        loss = -torch.mul(discounted_returns, action_log_probs).mean()
         loss.backward()
         self.policy_optimizer.step()
-
         self.clear_history()
+        return
